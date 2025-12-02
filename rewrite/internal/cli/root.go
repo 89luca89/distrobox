fe1f60b074a81e785d512a7c9fd7cef0725aba3b
Fabrizio Sestito <fabrizio.sestito@suse.com>
2025-12-02 15:50:03 +0100

    feat: validate sudo before command (#10)
    * feat(cli): validate sudo (sudo -v) in beforeCommand if --root is set
    
    Signed-off-by: Fabrizio Sestito <fabrizio.sestito@suse.com>
    
    * feat(docker): add root and sudoCommand and execute commands with sudo if root is set
    
    Signed-off-by: Fabrizio Sestito <fabrizio.sestito@suse.com>
    
    * feat(cli): add --root description and -r alias
    
    Signed-off-by: Fabrizio Sestito <fabrizio.sestito@suse.com>
    
    ---------
    
    Signed-off-by: Fabrizio Sestito <fabrizio.sestito@suse.com>

--- rewrite/internal/cli/root.go
+++ rewrite/internal/cli/root.go
@@ -2,6 +2,9 @@ package cli
 
 import (
 	"context"
+	"fmt"
+	"os"
+	"os/exec"
 
 	"github.com/urfave/cli/v3"
 
@@ -32,6 +35,13 @@ func NewRootCommand() *cli.Command {
 				Value:   "sudo",
 			},
 			&cli.BoolFlag{
+				Name: "root",
+				Usage: "launch podman/docker/lilipod with root privileges. Note that if you need root this is the preferred\n" +
+					"way over \"sudo distrobox\" (note: if using a program other than 'sudo' for root privileges is necessary,\n" +
+					"specify it through the DBX_SUDO_PROGRAM env variable, or 'distrobox_sudo_program' config variable)",
+				Aliases: []string{"r"},
+			},
+			&cli.BoolFlag{
 				Name:    "verbose",
 				Aliases: []string{"v"},
 				Usage:   "show more verbosity",
@@ -46,16 +56,36 @@ func NewRootCommand() *cli.Command {
 }
 
 func beforeAction(ctx context.Context, cmd *cli.Command) (context.Context, error) {
+	root := cmd.Bool("root")
+	if root {
+		if err := validateSudo(ctx); err != nil {
+			return nil, fmt.Errorf("cannot run in root mode: %w", err)
+		}
+	}
+	sudoCommand := cmd.String("sudo-command")
 	containerManagerType := cmd.String("container-manager")
 	verbose := cmd.Bool("verbose")
 
 	var containerManager containermanager.ContainerManager
 	switch containerManagerType {
 	case "docker":
-		containerManager = providers.NewDocker(verbose)
+		containerManager = providers.NewDocker(root, sudoCommand, verbose)
 	default:
-		containerManager = providers.NewDocker(verbose)
+		containerManager = providers.NewDocker(root, sudoCommand, verbose)
 	}
 
 	return context.WithValue(ctx, contextKey("containerManager"), containerManager), nil
+}
+
+func validateSudo(ctx context.Context) error {
+	cmd := exec.CommandContext(ctx, "sudo", "-v")
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	if err := cmd.Run(); err != nil {
+		return fmt.Errorf("failed to validate sudo: %w", err)
+	}
+
+	return nil
 }
