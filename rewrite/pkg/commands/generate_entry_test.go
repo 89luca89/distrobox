4a7d83bfe7f20a1c5fe5703f7ee1075ae8cf0101
Emanuele De Cupis <emanuele@decup.is>
2025-12-02 16:22:14 +0100

    feat: generate-entry command (#7)
    * add GenerateEntryCommand package
    
    The package mimics the actual distrobox-generate-entry shell command
    that installs a desktop entry file so that the host's desktop
    environment can render the appropriate application icon.
    This first implementantion scaffolds the command package and implements
    the simplest scenario for a single container.
    
    * delete entry
    
    If the `delete` flag is provided, the command remove the desktop entry
    if present.
    Deleting a non-existing item does not raise an error.
    
    * generate entries for all containers
    
    With `opts.All` we implement an abstraction over the
    `GenerateEntryCommand` that iterates across all the containers to
    generate or delete the relative desktop entries.
    `ListCommand` is used to fetch the list of containers.
    
    * mount the generate-entry cli command
    
    Mount the command so that is reachable from the CLI. Define parameters
    with type and basic validation.
    Depending on the flag `--all`, the appropriate set of options is provided
    to the `GenerateEntryCommand.Execute()` method.
    If `--all` is set, `container-name` and `icon` arguments will be ignored.

---
+++ rewrite/pkg/commands/generate_entry_test.go
@@ -0,0 +1,224 @@
+package commands_test
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"testing"
+
+	"github.com/89luca89/distrobox/pkg/commands"
+	"github.com/89luca89/distrobox/pkg/containermanager/providers"
+)
+
+func TestGenerateEntryCommand_Execute(t *testing.T) {
+	ctx := context.Background()
+
+	tempDir := t.TempDir()
+	defer os.RemoveAll(tempDir)
+
+	// create the list command
+	containerManager := providers.NewDocker(false)
+	listCmd := commands.NewListCommand(containerManager)
+
+	//
+	// Generate the entry
+	//
+
+	generateEntryCmd := commands.NewGenerateEntryCommand(listCmd)
+
+	opts := &commands.GenerateEntryOptions{
+		ContainerName:       "test-container",
+		Verbose:             true,
+		Delete:              false,
+		Icon:                "https://raw.githubusercontent.com/89luca89/distrobox/main/icons/terminal-distrobox-icon.svg",
+		Root:                false,
+		DesktopEntryBaseDir: fmt.Sprintf("%s/.local/share/", tempDir),
+		DistroboxPath:       "/usr/bin/distrobox",
+	}
+
+	err := generateEntryCmd.Execute(ctx, opts)
+	if err != nil {
+		t.Errorf("GenerateEntryCommand.Execute() error = %v", err)
+	}
+
+	expectedEntryPath := fmt.Sprintf("%s/.local/share/applications/test-container.desktop", tempDir)
+	if _, err := os.Stat(expectedEntryPath); os.IsNotExist(err) {
+		t.Errorf("Expected desktop entry file %s does not exist, %s", expectedEntryPath, tempDir)
+	}
+
+	expectedContent := `[Desktop Entry]
+Name=Test-container
+GenericName=Terminal entering Test-container
+Comment=Terminal entering Test-container
+Categories=Distrobox;System;Utility
+Exec=/usr/bin/distrobox enter  test-container
+Icon=https://raw.githubusercontent.com/89luca89/distrobox/main/icons/terminal-distrobox-icon.svg
+Keywords=distrobox;
+NoDisplay=false
+Terminal=true
+TryExec=/usr/bin/distrobox
+Type=Application
+Actions=Remove;
+
+[Desktop Action Remove]
+Name=Remove Test-container from system
+Exec=/usr/bin/distrobox rm  test-container`
+
+	content, err := os.ReadFile(expectedEntryPath)
+	if err != nil {
+		t.Errorf("Failed to read desktop entry file: %v", err)
+	}
+
+	if string(content) != expectedContent {
+		t.Errorf(
+			"Desktop entry content does not match expected.\nGot:\n'%s'\nExpected:\n'%s'",
+			string(content),
+			expectedContent,
+		)
+	}
+
+	// Delete the entry
+	opts.Delete = true
+	err = generateEntryCmd.Execute(ctx, opts)
+	if err != nil {
+		t.Errorf("GenerateEntryCommand.Execute() error on delete = %v", err)
+	}
+
+	if _, err := os.Stat(expectedEntryPath); !os.IsNotExist(err) {
+		t.Errorf("Expected desktop entry file %s to be deleted, %s", expectedEntryPath, tempDir)
+	}
+
+	// Try deleting a non-existing entry
+	err = generateEntryCmd.Execute(ctx, opts)
+	if err != nil {
+		t.Errorf("GenerateEntryCommand.Execute() error on delete non-existing = %v", err)
+	}
+}
+
+func TestGenerateEntryCommand_Execute_Root(t *testing.T) {
+	ctx := context.Background()
+
+	tempDir := t.TempDir()
+	defer os.RemoveAll(tempDir)
+
+	containerManager := providers.NewDocker(false)
+	listCmd := commands.NewListCommand(containerManager)
+
+	generateEntryCmd := commands.NewGenerateEntryCommand(listCmd)
+
+	opts := &commands.GenerateEntryOptions{
+		ContainerName:       "test-container",
+		Verbose:             true,
+		Delete:              false,
+		Icon:                "https://raw.githubusercontent.com/89luca89/distrobox/main/icons/terminal-distrobox-icon.svg",
+		Root:                true,
+		DesktopEntryBaseDir: fmt.Sprintf("%s/.local/share/", tempDir),
+		DistroboxPath:       "/usr/bin/distrobox",
+	}
+
+	err := generateEntryCmd.Execute(ctx, opts)
+	if err != nil {
+		t.Errorf("GenerateEntryCommand.Execute() error = %v", err)
+	}
+
+	expectedEntryPath := fmt.Sprintf("%s/.local/share/applications/test-container.desktop", tempDir)
+	if _, err := os.Stat(expectedEntryPath); os.IsNotExist(err) {
+		t.Errorf("Expected desktop entry file %s does not exist, %s", expectedEntryPath, tempDir)
+	}
+
+	expectedContent := `[Desktop Entry]
+Name=Test-container
+GenericName=Terminal entering Test-container
+Comment=Terminal entering Test-container
+Categories=Distrobox;System;Utility
+Exec=/usr/bin/distrobox enter --root test-container
+Icon=https://raw.githubusercontent.com/89luca89/distrobox/main/icons/terminal-distrobox-icon.svg
+Keywords=distrobox;
+NoDisplay=false
+Terminal=true
+TryExec=/usr/bin/distrobox
+Type=Application
+Actions=Remove;
+
+[Desktop Action Remove]
+Name=Remove Test-container from system
+Exec=/usr/bin/distrobox rm --root test-container`
+
+	content, err := os.ReadFile(expectedEntryPath)
+	if err != nil {
+		t.Errorf("Failed to read desktop entry file: %v", err)
+	}
+
+	if string(content) != expectedContent {
+		t.Errorf(
+			"Desktop entry content does not match expected.\nGot:\n'%s'\nExpected:\n'%s'",
+			string(content),
+			expectedContent,
+		)
+	}
+}
+
+func TestGenerateAllEntriesCommand_Execute(t *testing.T) {
+	ctx := context.Background()
+
+	// tempDir is the test directory where we expect to create desktop entries
+	tempDir := t.TempDir()
+	defer os.RemoveAll(tempDir)
+
+	// create the list command
+	containerManager := providers.NewDocker(false)
+	listCmd := commands.NewListCommand(containerManager)
+
+	// create the generate all entries command
+	genAllEntriesCmd := commands.NewGenerateEntryCommand(listCmd)
+
+	//
+	// Generate the entries
+	//
+
+	opts := &commands.GenerateEntryOptions{
+		All:                 true,
+		Verbose:             false,
+		Delete:              false,
+		DesktopEntryBaseDir: fmt.Sprintf("%s/.local/share/", tempDir),
+		DistroboxPath:       "/usr/bin/distrobox",
+	}
+	err := genAllEntriesCmd.Execute(ctx, opts)
+
+	if err != nil {
+		t.Errorf("GenerateAllEntriesCommand.Execute() error = %v", err)
+	}
+
+	// retrieve the list of containers to verify entries were created
+	listResult, err := listCmd.Execute(ctx)
+	if err != nil {
+		t.Errorf("ListCommand.Execute() error = %v", err)
+	}
+
+	// verify that each container has a corresponding desktop entry
+	for _, container := range listResult.Containers {
+		expectedEntryPath := fmt.Sprintf("%s/.local/share/applications/%s.desktop", tempDir, container.Name)
+		if _, err := os.Stat(expectedEntryPath); os.IsNotExist(err) {
+			t.Errorf("Expected desktop entry file %s does not exist", expectedEntryPath)
+		}
+	}
+
+	//
+	// Delete the entries
+	//
+
+	opts.Delete = true
+	err = genAllEntriesCmd.Execute(ctx, opts)
+
+	if err != nil {
+		t.Errorf("GenerateAllEntriesCommand.Execute() error = %v", err)
+	}
+
+	// verify that each container's desktop entry has been deleted
+	for _, container := range listResult.Containers {
+		expectedEntryPath := fmt.Sprintf("%s/.local/share/applications/%s.desktop", tempDir, container.Name)
+		if _, err := os.Stat(expectedEntryPath); !os.IsNotExist(err) {
+			t.Errorf("Expected desktop entry file %s to be deleted", expectedEntryPath)
+		}
+	}
+}
