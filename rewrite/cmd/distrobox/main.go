6c625c07c623ed3790551a2d2cdac9029d4669c5
Fabrizio Sestito <fabrizio.sestito@suse.com>
2025-12-04 19:01:01 +0100

    feat: add config (#15)
    * feat: add config
    
    Signed-off-by: Fabrizio Sestito <fabrizio.sestito@suse.com>
    
    * build(deps): add testify
    
    Signed-off-by: Fabrizio Sestito <fabrizio.sestito@suse.com>
    
    * test(config): add config loading test
    
    Signed-off-by: Fabrizio Sestito <fabrizio.sestito@suse.com>
    
    ---------
    
    Signed-off-by: Fabrizio Sestito <fabrizio.sestito@suse.com>

--- rewrite/cmd/distrobox/main.go
+++ rewrite/cmd/distrobox/main.go
@@ -6,11 +6,15 @@ import (
 	"os"
 
 	"github.com/89luca89/distrobox/internal/cli"
+	"github.com/89luca89/distrobox/internal/config"
 )
 
 func main() {
-	cmd := cli.NewRootCommand()
+	if err := config.LoadConfig(); err != nil {
+		log.Fatal(err)
+	}
 
+	cmd := cli.NewRootCommand()
 	if err := cmd.Run(context.Background(), os.Args); err != nil {
 		log.Fatal(err)
 	}
