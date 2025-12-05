8ac07d57c1014b6d4944f385654110861b04cdb2
Emanuele De Cupis <emanuele@decup.is>
2025-12-05 10:38:09 +0100

    feat(create): Generate desktop entry on create (#17)
    * read desktop entry directory and distrobox path from the global config
    
    * implement entry generation requested from user

--- rewrite/internal/config/config.go
+++ rewrite/internal/config/config.go
@@ -26,6 +26,26 @@ func LoadConfig() error {
 	return nil
 }
 
+// GetDesktopEntryDir resolves the system path for the desktop entry file
+func GetDesktopEntryDir() string {
+	xdgDataHome := os.Getenv("XDG_DATA_HOME")
+	if xdgDataHome == "" {
+		home := os.Getenv("HOME")
+		return filepath.Join(home, ".local", "share")
+	}
+	return xdgDataHome
+}
+
+// GetDistroboxPath returns the path to the current distrobox executable
+func GetDistroboxPath() (string, error) {
+	// The current executable is used as distrobox path
+	distroboxPath, err := os.Executable()
+	if err != nil {
+		return "", fmt.Errorf("failed to get distrobox executable path: %w", err)
+	}
+	return distroboxPath, nil
+}
+
 // getConfigFilePaths returns a list of configuration file paths in order of priority.
 func getConfigFilePaths() ([]string, error) {
 	execPath, err := os.Executable()
