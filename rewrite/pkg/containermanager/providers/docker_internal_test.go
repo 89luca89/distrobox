6abb02416f45bd4abca532438f5b6e6bb9e8d0b3
Emanuele De Cupis <emanuele@decup.is>
2025-12-06 15:44:59 +0100

    fix(create): fix argument passing (#21)

--- rewrite/pkg/containermanager/providers/docker_internal_test.go
+++ rewrite/pkg/containermanager/providers/docker_internal_test.go
@@ -35,8 +35,8 @@ func TestDocker_makeCreateCommand(t *testing.T) {
 		"",                            // containerPlatform
 		false,                         // nopasswd
 		true,                          // init
-		"",                            // containerPreInitHook
-		"",                            // containerInitHook
+		"echo 'pre-init-hook'",        // containerPreInitHook
+		"echo 'init-hook'",            // containerInitHook
 		false,                         // nvidia
 		false,                         // unshareDevsys
 		false,                         // unshareGroups
@@ -50,20 +50,56 @@ func TestDocker_makeCreateCommand(t *testing.T) {
 	)
 
 	expected := oneline(`
-create --hostname my-hostname --name my-container --privileged --security-opt label=disable --security-opt
- apparmor=unconfined --pids-limit=-1 --user root:root --ipc host --network host --pid host --label manager=distrobox
- --label distrobox.unshare_groups=0 --env SHELL=sh --env HOME=/home/user --env container=docker
- --env TERMINFO_DIRS=/usr/share/terminfo:/run/host/usr/share/terminfo --env CONTAINER_ID=my-container
- --volume /tmp:/tmp:rslave --volume /path/to/distrobox-export:/usr/bin/distrobox-export:ro
- --volume /path/to/distrobox-hostexec:/usr/bin/distrobox-host-exec:ro --volume /home/user:/home/user:rslave
- --volume /:/run/host/:rslave --volume /dev:/dev:rslave --volume /sys:/sys:rslave --cgroupns host
- --stop-signal SIGRTMIN+3 --mount type=tmpfs,destination=/run --mount type=tmpfs,destination=/run/lock
- --mount type=tmpfs,destination=/var/lib/journal --volume /dev/pts --volume /dev/null:/dev/ptmx
- --volume /sys/fs/selinux --volume /var/log/journal --volume /etc/hosts:/etc/hosts:ro
- --volume /etc/resolv.conf:/etc/resolv.conf:ro --volume /path/to/my-volume:/var/local/my-volume:ro
- --volume /path/to/distrobox-init:/usr/bin/entrypoint:ro --entrypoint /usr/bin/entrypoint my-image --verbose
- --name my-container --user 1000 --group 1000 --home /home/user --init "1" --nvidia "0" --pre-init-hooks
- --additional-packages  -- ''
+ create
+ --hostname my-hostname
+ --name my-container
+ --privileged
+ --security-opt label=disable
+ --security-opt apparmor=unconfined
+ --pids-limit=-1
+ --user root:root
+ --ipc host
+ --network host
+ --pid host
+ --label manager=distrobox
+ --label distrobox.unshare_groups=0
+ --env SHELL=sh
+ --env HOME=/home/user
+ --env container=docker
+ --env TERMINFO_DIRS=/usr/share/terminfo:/run/host/usr/share/terminfo
+ --env CONTAINER_ID=my-container
+ --volume /tmp:/tmp:rslave
+ --volume /path/to/distrobox-export:/usr/bin/distrobox-export:ro
+ --volume /path/to/distrobox-hostexec:/usr/bin/distrobox-host-exec:ro
+ --volume /home/user:/home/user:rslave
+ --volume /:/run/host/:rslave
+ --volume /dev:/dev:rslave
+ --volume /sys:/sys:rslave
+ --cgroupns host
+ --stop-signal SIGRTMIN+3
+ --mount type=tmpfs,destination=/run
+ --mount type=tmpfs,destination=/run/lock
+ --mount type=tmpfs,destination=/var/lib/journal
+ --volume /dev/pts
+ --volume /dev/null:/dev/ptmx
+ --volume /sys/fs/selinux
+ --volume /var/log/journal
+ --volume /etc/hosts:/etc/hosts:ro
+ --volume /etc/resolv.conf:/etc/resolv.conf:ro
+ --volume /path/to/my-volume:/var/local/my-volume:ro
+ --volume /path/to/distrobox-init:/usr/bin/entrypoint:ro
+ --entrypoint /usr/bin/entrypoint
+ my-image
+ --verbose
+ --name user
+ --user 1000
+ --group 1000
+ --home /home/user
+ --init 1
+ --nvidia 0
+ --pre-init-hooks echo 'pre-init-hook'
+ --additional-packages 
+ -- echo 'init-hook'
 `)
 
 	got := oneline(strings.Join(cmd, " "))
