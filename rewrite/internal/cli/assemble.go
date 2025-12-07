817862280167d34b861f6399ccd86e8e7b87f1fe
Emanuele De Cupis <emanuele@decup.is>
2025-12-07 23:00:14 +0100

    feat(assemble): implement create with assemble (#20)
    * scaffold assemble command
    
    * mount assemble command
    
    * implement create item

---
+++ rewrite/internal/cli/assemble.go
@@ -0,0 +1,115 @@
+package cli
+
+import (
+	"context"
+	"errors"
+	"fmt"
+
+	"github.com/urfave/cli/v3"
+
+	"github.com/89luca89/distrobox/pkg/commands"
+	"github.com/89luca89/distrobox/pkg/containermanager"
+	"github.com/89luca89/distrobox/pkg/manifest"
+)
+
+func newAssembleCommand() *cli.Command {
+	fileFlag := &cli.StringFlag{Name: "file", Usage: "path or URL to the distrobox manifest/ini file"}
+	nameFlag := &cli.StringFlag{
+		Name:    "name",
+		Aliases: []string{"n"},
+		Usage:   "run against a single entry in the manifest/ini file",
+	}
+	dryRunFlag := &cli.BoolFlag{
+		Name:    "dry-run",
+		Aliases: []string{"d"},
+		Usage:   "only print the container manager command generated",
+	}
+
+	return &cli.Command{
+		Name: "assemble",
+		UsageText: `
+	distrobox assemble create
+	distrobox assemble rm
+	distrobox assemble create --file /path/to/file.ini
+	distrobox assemble rm --file /path/to/file.ini
+	distrobox assemble create --replace --file /path/to/file.ini
+
+Options:
+
+	--file:			path or URL to the distrobox manifest/ini file
+	--name/-n:		run against a single entry in the manifest/ini file
+	--replace/-R:		replace already existing distroboxes with matching names
+	--dry-run/-d:		only print the container manager command generated
+	--verbose/-v:		show more verbosity
+	--version/-V:		show version
+`,
+		Commands: []*cli.Command{
+			{
+				Name: "create",
+				Flags: []cli.Flag{
+					fileFlag,
+					nameFlag,
+					&cli.BoolFlag{
+						Name:    "replace",
+						Aliases: []string{"R"},
+						Usage:   "replace already existing distroboxes with matching names",
+					},
+					dryRunFlag,
+				},
+				Action: func(ctx context.Context, cmd *cli.Command) error {
+					return assembleAction(ctx, cmd, false)
+				},
+			},
+			{
+				Name: "rm",
+				Flags: []cli.Flag{
+					fileFlag,
+					nameFlag,
+					dryRunFlag,
+				},
+				Action: func(ctx context.Context, cmd *cli.Command) error {
+					return assembleAction(ctx, cmd, true)
+				},
+			},
+		},
+	}
+}
+
+func assembleAction(ctx context.Context, cmd *cli.Command, deleteFlag bool) error {
+	containerManager, ok := ctx.Value(containerManagerKey).(containermanager.ContainerManager)
+	if !ok {
+		return errors.New("container manager not found in context")
+	}
+
+	// TODO: handle file name as a positional argument
+	// https://github.com/89luca89/distrobox-next/blob/07b3abf2015effafc5596b9dc7f02c35a17eb8a7/distrobox-assemble#L205
+
+	manifestFilePath := cmd.String("file")
+	if manifestFilePath == "" {
+		manifestFilePath = "./distrobox.ini"
+	}
+
+	manifest, err := manifest.Parse(manifestFilePath)
+	if err != nil {
+		return fmt.Errorf("failed to parse manifest file: %w", err)
+	}
+
+	opts := commands.AssembleOptions{
+		Items:   manifest,
+		Boxname: cmd.String("name"),
+		DryRun:  cmd.Bool("dry-run"),
+	}
+	if deleteFlag {
+		opts.Delete = true
+	} else {
+		opts.Replace = cmd.Bool("replace")
+	}
+
+	assembleCmd := commands.NewAssembleCommand(containerManager)
+
+	err = assembleCmd.Execute(ctx, opts)
+	if err != nil {
+		return fmt.Errorf("failed to execute assemble command: %w", err)
+	}
+	return nil
+}
---
+++ rewrite/pkg/commands/assemble.go
@@ -0,0 +1,133 @@
+package commands
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"regexp"
+	"slices"
+	"strings"
+
+	"github.com/89luca89/distrobox/pkg/containermanager"
+	"github.com/89luca89/distrobox/pkg/manifest"
+)
+
+type AssembleOptions struct {
+	Items []manifest.Item
+	// Boxname is the name of the box to assemble
+	// If specified, the Assemble command will only assemble the given box
+	// If empty, the command will assemble all boxes defined in the manifest
+	Boxname string
+	// Delete indicates whether to delete the existing box before assembling
+	// true=delete, false=create or update
+	Delete  bool
+	Replace bool
+	Verbose bool
+	DryRun  bool
+}
+
+type AssembleCommand struct {
+	containermanager containermanager.ContainerManager
+	createCmd        *CreateCommand
+}
+
+func NewAssembleCommand(cm containermanager.ContainerManager) *AssembleCommand {
+	return &AssembleCommand{
+		containermanager: cm,
+		createCmd:        NewCreateCommand(cm),
+	}
+}
+
+func (ac *AssembleCommand) Execute(ctx context.Context, opts AssembleOptions) error {
+	var items []manifest.Item
+	if opts.Boxname != "" {
+		idx := slices.IndexFunc(opts.Items, func(i manifest.Item) bool {
+			return i.Name == opts.Boxname
+		})
+		if idx == -1 {
+			return fmt.Errorf("box '%s' not found in manifest", opts.Boxname)
+		}
+		items = []manifest.Item{opts.Items[idx]}
+	} else {
+		items = opts.Items
+	}
+
+	for _, item := range items {
+		switch {
+		case opts.Delete:
+			if err := ac.deleteItem(ctx, item, opts.DryRun); err != nil {
+				return fmt.Errorf("failed to delete item '%s': %w", item.Name, err)
+			}
+		case opts.Replace:
+			if err := ac.replaceItem(ctx, item, opts.DryRun); err != nil {
+				return fmt.Errorf("failed to replace item '%s': %w", item.Name, err)
+			}
+		default:
+			if err := ac.createItem(ctx, item, opts.DryRun); err != nil {
+				return fmt.Errorf("failed to create item '%s': %w", item.Name, err)
+			}
+		}
+	}
+
+	return nil
+}
+
+func (ac *AssembleCommand) deleteItem(_ context.Context, _ manifest.Item, _ bool) error {
+	return errors.New("rm not implemented yet")
+}
+
+func (ac *AssembleCommand) replaceItem(_ context.Context, _ manifest.Item, _ bool) error {
+	return errors.New("replace not implemented yet")
+}
+
+func (ac *AssembleCommand) createItem(ctx context.Context, item manifest.Item, dryRun bool) error {
+	opts := CreateOptions{
+		ContainerClone:          item.Clone,
+		ContainerName:           item.Name,
+		ContainerImage:          item.Image,
+		ContainerHostname:       item.Hostname,
+		UnshareNetNs:            item.UnshareNetns,
+		UnshareDevsys:           item.UnshareDevsys,
+		UnshareGroups:           item.UnshareGroups,
+		UnshareIpc:              item.UnshareIPC,
+		UnshareProcess:          item.UnshareProcess,
+		AdditionalFlags:         item.AdditionalFlags,
+		AdditionalVolumes:       item.Volumes,
+		AdditionalPackages:      item.AdditionalPackages,
+		ContainerPreInitHook:    ac.joinHooks(item.PreInitHooks),
+		ContainerInitHook:       ac.joinHooks(item.InitHooks),
+		ContainerUserCustomHome: item.Home,
+		Init:                    item.Init,
+		Nvidia:                  item.Nvidia,
+		GenerateEntry:           item.Entry,
+		Rootful:                 item.Root,
+		DryRun:                  dryRun,
+	}
+
+	// TODO: pull image if needed
+	// https://github.com/89luca89/distrobox/blob/main/distrobox-create#L1016
+
+	return ac.createCmd.Execute(ctx, opts)
+}
+
+func (ac *AssembleCommand) joinHooks(hooks []string) string {
+	sb := strings.Builder{}
+
+	for i, hook := range hooks {
+		sb.WriteString(hook)
+
+		if i < len(hooks)-1 {
+			semicolonRegex := regexp.MustCompile(`;[[:space:]]{0,1}$`)
+			andAndRegex := regexp.MustCompile(`&&[[:space:]]{0,1}$`)
+
+			separator := "  " // two spaces just because v1 does that, so it's comparable in regression tests
+			if !semicolonRegex.MatchString(hook) && !andAndRegex.MatchString(hook) {
+				separator = " && "
+			}
+
+			sb.WriteString(separator)
+		}
+	}
+
+	return sb.String()
+}
