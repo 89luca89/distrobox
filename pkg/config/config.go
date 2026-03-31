4b22fa130dda895e026d8dbb58af60b43b3fa783
balanza <emanuele@decup.is>
2026-03-31 16:47:32 +0200

    refactor(config): expose default config values

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -7,3 +7,32 @@ type Values struct {
 	DefaultContainerImage string
 	DefaultContainerName  string
 }
+
+func defaultsMap() map[string]string {
+	return map[string]string{
+		"container_manager": "podman",
+		"sudo_program":      "sudo",
+		"verbose":           "false",
+		// container_image Fedora toolbox is a sensitive default
+		"container_image": "registry.fedoraproject.org/fedora-toolbox:latest",
+		"container_name":  "my-distrobox",
+	}
+}
+
+func DefaultValues() *Values {
+	return toStruct(defaultsMap())
+}
+
+func toStruct(configMap map[string]string) *Values {
+	return &Values{
+		ContainerManagerType:  configMap["container_manager"],
+		SudoProgram:           configMap["sudo_program"],
+		Verbose:               toBool(configMap["verbose"]),
+		DefaultContainerImage: configMap["container_image"],
+		DefaultContainerName:  configMap["container_name"],
+	}
+}
+
+func toBool(value string) bool {
+	return value == "true"
+}
