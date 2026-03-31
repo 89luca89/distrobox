724f2fa429ab2d38fb63ae5e8c830a046b69c2a2
balanza <emanuele@decup.is>
2026-03-31 16:46:16 +0200

    refactor(config): public ConfigValue struct type

---
+++ pkg/config/config.go
@@ -0,0 +1,9 @@
+package config
+
+type Values struct {
+	ContainerManagerType  string
+	SudoProgram           string
+	Verbose               bool
+	DefaultContainerImage string
+	DefaultContainerName  string
+}
