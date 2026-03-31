0f9a1beb1eaebd8e48184ecdc408a76277e44233
balanza <emanuele@decup.is>
2026-03-31 19:42:40 +0200

    refactor(config): move XDG_DATA_HOME read into userenv

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,16 +1,1 @@
 package config
-
-import (
-	"os"
-	"path/filepath"
-)
-
-// GetDesktopEntryDir resolves the system path for the desktop entry file
-func GetDesktopEntryDir() string {
-	xdgDataHome := os.Getenv("XDG_DATA_HOME")
-	if xdgDataHome == "" {
-		home := os.Getenv("HOME")
-		return filepath.Join(home, ".local", "share")
-	}
-	return xdgDataHome
-}
