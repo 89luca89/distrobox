b51afb2e0749e5a6bc83b855524179d421ebc4df
Fabrizio Sestito <fabrizio.sestito@suse.com>
2026-04-01 09:36:45 +0200

    feat(containermanager): add autodetect provider constructor
    Signed-off-by: Fabrizio Sestito <fabrizio.sestito@suse.com>

---
+++ pkg/containermanager/providers/autodetect.go
@@ -0,0 +1,26 @@
+package providers
+
+import (
+	"errors"
+	"os/exec"
+
+	"github.com/89luca89/distrobox/pkg/containermanager"
+)
+
+// ErrNoContainerManager is returned when no supported container runtime is found.
+var ErrNoContainerManager = errors.New("no container manager found")
+
+// NewAutoDetect returns a ContainerManager for the first available container runtime.
+// Priority order: podman > podman-launcher > docker.
+func NewAutoDetect(root bool, sudoCommand string, verbose bool) (containermanager.ContainerManager, error) {
+	if _, err := exec.LookPath("podman"); err == nil {
+		return NewPodman(root, sudoCommand, verbose), nil
+	}
+	if _, err := exec.LookPath("podman-launcher"); err == nil {
+		return NewPodman(root, sudoCommand, verbose), nil
+	}
+	if _, err := exec.LookPath("docker"); err == nil {
+		return NewDocker(root, sudoCommand, verbose), nil
+	}
+	return nil, ErrNoContainerManager
+}
